6d6b2478bb8291596a115dbbd77971ef60c20fc9
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-16 19:52:54 +0800

    fix: reverted to og files

--- src/pages/api/getSocials.ts
+++ src/pages/api/getSocials.ts
@@ -1,6 +1,6 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { groq } from 'next-sanity';
-import { sanityClient } from '@lib/sanity';
+import { sanityClient } from '@sanity';
 
 const query = groq`*[_type=="social"]`;
 

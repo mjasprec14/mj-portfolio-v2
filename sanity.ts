0ff9d9d78944629222b4c1e7eacaed3e5d6d5a0e
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-16 17:15:33 +0800

    fix: using env variables for config

--- sanity.ts
+++ sanity.ts
@@ -1,10 +1,10 @@
 import { createClient } from 'next-sanity';
 
 export const config = {
-  projectId: 'hewoqwvv',
-  dataset: 'production',
+  projectId: process.env.SANITY_PROJECT_ID,
+  dataset: process.env.SANITY_DATASET_NAME || 'production',
   apiVersion: '2022-03-25',
-  useCdn: false,
+  useCdn: process.env.NODE_ENV === 'production',
 };
 
 export const sanityClient = createClient(config);

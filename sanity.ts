22bd2de473a60da6eb9e1ffc475dac7d629ef7df
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-16 16:29:37 +0800

    feat: sanity backend initial setup

---
+++ sanity.ts
@@ -0,0 +1,10 @@
+import { createClient } from 'next-sanity';
+
+export const config = {
+  projectId: 'hewoqwvv',
+  dataset: 'production',
+  apiVersion: '2022-03-25',
+  useCdn: false,
+};
+
+export const client = createClient(config);
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Header } from './Header/Header';
 export { default as Navbar } from './Navbar/Navbar';
 export { default as Logo } from './Logo/Logo';
+export { default as Layout } from './Layout/Layout';

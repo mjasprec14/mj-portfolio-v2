7ae7199fde5f5318b75fee94050d8f7570b1130f
mjasprec <michaeljosephasprec1@gmail.com>
2023-04-16 16:28:24 +0800

    feat: getStaticProps initial

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,23 @@
-import {} from '@components/index';
+import { Layout } from '@components/index';
+import { fetchSocials } from '@utils/fetchSocials';
 
-export default function Home() {
+export default function Home({ socials }) {
+  console.log('socials', socials);
   return (
     <>
-      <main className=''></main>
+      <main className='flex items-center text-dark w-full min-h-screen'>
+        <Layout />
+      </main>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const socials = await fetchSocials();
+
+  return {
+    props: {
+      socials,
+    },
+  };
 }
